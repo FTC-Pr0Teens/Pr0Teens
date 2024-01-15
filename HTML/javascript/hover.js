d286e3fa6e7d2d9a1a1a65dacd4cde8bc0cdafbe
Robotli89 <121524712+Robotli89@users.noreply.github.com>
2024-01-15 10:33:58 -0500

    Navbar Hover effect

--- HTML/javascript/hover.js
+++ HTML/javascript/hover.js
@@ -1,9 +1,14 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
+  var navbar = document.getElementById("navbar");
+
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
+      navbar.style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+      navbar.style.top = "-50px";
   }
 }
+
